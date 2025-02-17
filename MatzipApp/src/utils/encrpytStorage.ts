319ce6c0a0255f542731b43520fb7d0efc4c2ec6
Lucid Jeon <nohack-@naver.com>
2025-02-17 22:24:53 +0900

    [feat] EncrpytStorage 유틸 추가

---
+++ MatzipApp/src/utils/encrpytStorage.ts
@@ -0,0 +1,17 @@
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+export const setEncryptStorage = async <T>(key: string, data: T) => {
+  await EncryptedStorage.setItem(key, JSON.stringify(data));
+};
+
+export const getEncryptStorage = async <T>(key: string): Promise<T | null> => {
+  const storedData = await EncryptedStorage.getItem(key);
+  return storedData ? JSON.parse(storedData) : null;
+};
+
+export const removeEncryptStorage = async (key: string) => {
+  const data = await getEncryptStorage(key);
+  if (data) {
+    await EncryptedStorage.removeItem(key);
+  }
+};
