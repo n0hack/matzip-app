894ce618aa0c7b6a0bad347ab2ddc98c55501956
Lucid Jeon <nohack-@naver.com>
2025-02-17 21:10:23 +0900

    [feat] InputField 컴포넌트

---
+++ MatzipApp/src/components/input-field.tsx
@@ -0,0 +1,81 @@
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from 'react-native';
+import {colors} from '../constants';
+import {useRef} from 'react';
+
+interface InputFieldProps extends TextInputProps {
+  disabled?: boolean;
+  error?: string;
+  touched?: boolean;
+}
+
+const devicedHeight = Dimensions.get('screen').height;
+
+export function InputField({
+  error,
+  disabled = false,
+  touched = false,
+  ...rest
+}: InputFieldProps) {
+  const innerRef = useRef<TextInput>(null);
+
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+
+  return (
+    <Pressable onPress={handlePressInput}>
+      <View
+        style={[
+          styles.conatiner,
+          disabled && styles.disabled,
+          touched && Boolean(error) && styles.inputError,
+        ]}>
+        <TextInput
+          ref={innerRef}
+          editable={!disabled}
+          placeholderTextColor={colors.GRAY_500}
+          style={[styles.input, disabled && styles.disabled]}
+          autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
+          {...rest}
+        />
+        {touched && Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  conatiner: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    padding: devicedHeight > 700 ? 15 : 10,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.BLACK,
+    padding: 0,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  error: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+  },
+});
--- MatzipApp/src/screens/auth/login.tsx
+++ MatzipApp/src/screens/auth/login.tsx
@@ -1,17 +1,62 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {InputField} from '../../components/input-field';
+import {useState} from 'react';
 
 type LoginScreenProps = {};
 
 export default function LoginScreen({}: LoginScreenProps) {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeValues = (key: keyof typeof values) => (text: string) => {
+    setValues({...values, [key]: text});
+  };
+
+  const handleBlur = (key: keyof typeof values) => () => {
+    setTouched({
+      ...touched,
+      [key]: true,
+    });
+  };
+
   return (
-    <SafeAreaView>
-      <View style={styles.conatiner}>
-        <Text>로그인 스크린</Text>
+    <SafeAreaView style={styles.conatiner}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error="이메일을 입력하세요"
+          inputMode="email"
+          value={values.email}
+          onChangeText={handleChangeValues('email')}
+          onBlur={handleBlur('email')}
+          touched={touched.email}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error="비밀번호를 입력하세요"
+          secureTextEntry
+          value={values.password}
+          onChangeText={handleChangeValues('password')}
+          onBlur={handleBlur('password')}
+          touched={touched.password}
+        />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  conatiner: {},
+  conatiner: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
 });
