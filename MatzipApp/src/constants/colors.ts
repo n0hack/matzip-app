02fc47895ee9f117bbc2b2d261c9f080544bd23a
Lucid Jeon <nohack-@naver.com>
2025-02-23 21:53:25 +0900

    [feat] Drawer 커스텀

--- MatzipApp/src/constants/colors.ts
+++ MatzipApp/src/constants/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
   WHITE: '#FFFFFF',
   BLACK: '#000000',
 
+  GRAY_100: '#F8F8F8',
   GRAY_200: '#E7E7E7',
   GRAY_500: '#8E8E8E',
   GRAY_700: '#575757',
@@ -9,6 +10,7 @@ export const colors = {
   RED_300: '#FFB4B4',
   RED_500: '#FF5F5F',
 
+  PINK_200: '#FAE2E9',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
 } as const;
--- MatzipApp/src/navigations/drawer/main-drawer-navigator.tsx
+++ MatzipApp/src/navigations/drawer/main-drawer-navigator.tsx
@@ -5,9 +5,11 @@ import {
   MapStackNavigator,
   MapStackParamList,
 } from '../stack/map-stack-navigator';
-import { mainNavigations } from '@/constants';
-import { NavigatorScreenParams } from '@react-navigation/native';
-
+import { colors, mainNavigations } from '@/constants';
+import { NavigatorScreenParams, RouteProp } from '@react-navigation/native';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import { Dimensions } from 'react-native';
+import { CustomDrawerContent } from './custom-drawer-content';
 export type MainDrawerParamList = {
   [mainNavigations.HOME]: NavigatorScreenParams<MapStackParamList>;
   [mainNavigations.FEED]: undefined;
@@ -16,17 +18,60 @@ export type MainDrawerParamList = {
 
 const Drawer = createDrawerNavigator<MainDrawerParamList>();
 
+function DrawerIcons({
+  route,
+  focused,
+}: {
+  route: RouteProp<MainDrawerParamList, keyof MainDrawerParamList>;
+  focused: boolean;
+}) {
+  let iconName: any = '';
+
+  switch (route.name) {
+    case mainNavigations.HOME:
+      iconName = 'location-on';
+      break;
+    case mainNavigations.FEED:
+      iconName = 'book';
+      break;
+    case mainNavigations.CALENDAR:
+      iconName = 'event-note';
+      break;
+  }
+
+  return (
+    <MaterialIcons
+      name={iconName}
+      size={18}
+      color={focused ? colors.BLACK : colors.GRAY_500}
+    />
+  );
+}
+
 export function MainDrawerNavigator() {
   return (
     <Drawer.Navigator
-      screenOptions={{
+      drawerContent={CustomDrawerContent}
+      screenOptions={({ route }) => ({
         drawerType: 'front',
         headerShown: false,
-      }}>
+        drawerIcon: ({ focused }) => DrawerIcons({ route, focused }),
+        drawerStyle: {
+          width: Dimensions.get('screen').width * 0.6,
+          backgroundColor: colors.WHITE,
+        },
+        drawerActiveTintColor: colors.BLACK,
+        drawerInactiveTintColor: colors.GRAY_500,
+        drawerActiveBackgroundColor: colors.PINK_200,
+        drawerInactiveBackgroundColor: colors.GRAY_100,
+        drawerLabelStyle: {
+          fontWeight: 600,
+        },
+      })}>
       <Drawer.Screen
         name={mainNavigations.HOME}
         component={MapStackNavigator}
-        options={{ title: '홈' }}
+        options={{ title: '홈', swipeEnabled: false }}
       />
       <Drawer.Screen
         name={mainNavigations.FEED}
